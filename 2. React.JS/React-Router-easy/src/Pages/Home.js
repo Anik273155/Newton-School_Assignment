654f641a9948dd14ec5804e885a18a0139009470
Umesh Kumar Chaurasiya <113828943+Anik273155@users.noreply.github.com>
2023-01-29 17:06:26 +0530

    aaa
    Signed-off-by: Umesh Kumar Chaurasiya  <113828943+Anik273155@users.noreply.github.com>

--- 2. React.JS/React-Router-easy/src/Pages/Home.js
+++ 2. React.JS/React-Router-easy/src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Home = () => {
     return (
         <div id="home-page">
-            <h1>Home Page</h1>
+        <h1>Home Page</h1>
         </div>
     )
 }
